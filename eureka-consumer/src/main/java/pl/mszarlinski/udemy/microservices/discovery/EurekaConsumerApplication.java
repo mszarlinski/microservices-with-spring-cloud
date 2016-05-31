24d12244d11d377c975f2afdb0091b135a106655
Maciej Szarlinski <mszarlinski@bravurasolutions.com>
2016-05-31 12:07:09 +0200

    Ribbon - using @LoadBalanced example

--- eureka-consumer/src/main/java/pl/mszarlinski/udemy/microservices/discovery/EurekaConsumerApplication.java
+++ eureka-consumer/src/main/java/pl/mszarlinski/udemy/microservices/discovery/EurekaConsumerApplication.java
@@ -3,7 +3,10 @@ package pl.mszarlinski.udemy.microservices.discovery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
@@ -12,5 +15,11 @@ public class EurekaConsumerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaConsumerApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
--- eureka-consumer/src/main/java/pl/mszarlinski/udemy/microservices/discovery/GreetingController.java
+++ eureka-consumer/src/main/java/pl/mszarlinski/udemy/microservices/discovery/GreetingController.java
@@ -1,13 +1,10 @@
 package pl.mszarlinski.udemy.microservices.discovery;
 
-import java.net.URI;
 import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -24,20 +21,17 @@ public class GreetingController {
 
     private static final String DEFAULT_GREETING = "Default greeting";
 
-    private final LoadBalancerClient loadBalancerClient;
-
-    private final RestTemplate restTemplate = new RestTemplate();
+    private final RestTemplate restTemplate;
 
     @Autowired
-    public GreetingController(final LoadBalancerClient loadBalancerClient) {
-        this.loadBalancerClient = loadBalancerClient;
+    public GreetingController(final RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
     @RequestMapping("/greeting")
     public String greeting() {
         try {
-            return getProducerServiceUri()
-                .map(uri -> restTemplate.getForObject(uri + "/name", String.class))
+            return Optional.ofNullable(restTemplate.getForObject("http://" + PRODUCER_SERVICE_ID + "/name", String.class))
                 .map(this::createGreeting)
                 .orElse(DEFAULT_GREETING);
         } catch (Exception ex) {
@@ -48,11 +42,6 @@ public class GreetingController {
 
     private String createGreeting(final String name) {
         return "Hi, " + name;
-    }
-
-    private Optional<URI> getProducerServiceUri() {
-        return Optional.ofNullable(loadBalancerClient.choose(PRODUCER_SERVICE_ID))
-            .map(ServiceInstance::getUri);
     }
 
 }
