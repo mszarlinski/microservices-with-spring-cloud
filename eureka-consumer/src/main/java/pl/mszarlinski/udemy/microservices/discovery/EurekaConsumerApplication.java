48d3c2a520d6d026b9adeae37c452923e88f62fa
Maciej Szarlinski <mszarlinski@bravurasolutions.com>
2016-06-03 14:56:21 +0200

    Enable /hystrix.stream endpoint and Hystrix Dashboard

--- eureka-consumer/src/main/java/pl/mszarlinski/udemy/microservices/discovery/EurekaConsumerApplication.java
+++ eureka-consumer/src/main/java/pl/mszarlinski/udemy/microservices/discovery/EurekaConsumerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableHystrix
+@EnableHystrixDashboard
 public class EurekaConsumerApplication {
 
     public static void main(String[] args) {
